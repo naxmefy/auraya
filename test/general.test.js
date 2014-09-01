acd517cd1471d573ce5231fca08f54dc9778a3e1
MRW Neundorf <Naxmeify@users.noreply.github.com>
2014-09-01 14:14:41 +0200

    Create general.test.js

---
+++ test/general.test.js
@@ -0,0 +1,11 @@
+describe('API General Test', function () {
+  it('should', function (done) {
+    done()
+  });
+  it.skip('should do', function (done) {
+    done()
+  });
+  it.skip('should do too', function (done) {
+    done()
+  });
+});
